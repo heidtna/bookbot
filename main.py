0f81d50ed5ff4f4f1d28f7a263cf4ded9bc4fb9b
Nathan <heidtna@gmail.com>
2025-04-18 17:02:52 -0500

    add abilty to use book path at command line

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import get_num_words, get_character_instances, print_full_report
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    path_to_book = sys.argv[1]
     book_text = get_book_text(path_to_book)             # string
     word_count = get_num_words(book_text)               # int
     letter_count = get_character_instances(book_text)   # dictionary
